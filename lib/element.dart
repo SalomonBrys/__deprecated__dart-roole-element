020a52bd5753dee54641968d47610031f137074a
Salomon BRYS <salomon.brys@gmail.com>
2013-10-17 17:41:11 +0200

    error handling fix

--- lib/element.dart
+++ lib/element.dart
@@ -39,11 +39,12 @@ abstract class RooleElementMixin implements CustomElement {
     var comp = new Completer();
     if (encaps)
       txt = this.tagName.toLowerCase() + ", [is=" + this.tagName.toLowerCase() + "] {\n" + txt + "\n}\n";
-    js.context.roole.compile(txt, null, new js.Callback.once((error, css) {
+    js.context.roole.compile(txt, null, new js.Callback.once((error, [css]) {
       if (error != null) {
         window.console.log(error.toString());
-        return ;
       }
+      if (css == null)
+        return ;
       if (style == null)
         style = new StyleElement()
           ..type = "text/css"
