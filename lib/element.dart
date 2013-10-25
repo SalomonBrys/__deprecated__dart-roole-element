35d229e17311967946938b420352ad6680e152a6
Salomon BRYS <salomon.brys@gmail.com>
2013-10-25 09:40:10 +0200

    Polymer does the encapsulation

--- lib/element.dart
+++ lib/element.dart
@@ -5,7 +5,6 @@ import 'dart:html';
 import 'dart:js' as djs;
 import 'dart:async';
 
-import 'package:custom_element/custom_element.dart';
 import 'package:polymer/polymer.dart';
 import 'package:js/js.dart' as js;
 
@@ -15,30 +14,31 @@ class _CachedStyle {
   _CachedStyle(this.style);
 }
 
-abstract class RooleElementMixin implements CustomElement {
-  
-  bool _needEncapsulation() {
-    if (js.context == null) return false;
-    
+abstract class RooleElementMixin implements PolymerElement {
+
+  String _shimShadowDomStyling(String txt) {
+    if (djs.context == null) return txt;
+
     var platform = djs.context['Platform'];
-    if (platform == null) return false;
+    if (platform == null) return txt;
 
     var shadowCss = platform['ShadowCSS'];
-    if (shadowCss == null) return false;
+    if (shadowCss == null) return txt;
 
     var shimShadowDOMStyling2 = shadowCss['shimShadowDOMStyling2'];
-    if (shimShadowDOMStyling2 == null) return false;
-    
-    if (js.context.window.ShadowDOMPolyfill != null)
-      return true;
-    
-    return false;
+    if (shimShadowDOMStyling2 == null) return txt;
+
+//    if (js.context.window.ShadowDOMPolyfill == null) return false;
+//
+//    return true;
+
+    return shimShadowDOMStyling2.apply(shadowCss, [txt, localName]);
   }
-  
-  Future<StyleElement> _compileRoole(String txt, bool encaps, [StyleElement style]) {
+
+  Future<StyleElement> _compileRoole(String txt, [StyleElement style]) {
     var comp = new Completer();
-    if (encaps)
-      txt = this.tagName.toLowerCase() + ", [is=" + this.tagName.toLowerCase() + "] {\n" + txt + "\n}\n";
+//    if (encaps)
+//      txt = this.tagName.toLowerCase() + ", [is=" + this.tagName.toLowerCase() + "] {\n" + txt + "\n}\n";
     js.context.roole.compile(txt, js.map({'base': './'}), new js.Callback.once((error, [css]) {
       if (error != null) {
         window.console.log(error.toString());
@@ -49,14 +49,14 @@ abstract class RooleElementMixin implements CustomElement {
         style = new StyleElement()
           ..type = "text/css"
         ;
-      style.text = css;
+      style.text = _shimShadowDomStyling(css);
       shadowRoot.append(style);
       comp.complete(style);
     }));
     return comp.future;
   }
 
-  Future<_CachedStyle> _compileRooleHTTP(Element e, bool encaps, [_CachedStyle cache]) {
+  Future<_CachedStyle> _compileRooleHTTP(Element e, [_CachedStyle cache]) {
     return HttpRequest.getString(e.attributes["src"]).then((String text) {
       StyleElement style = null;
       if (cache != null) {
@@ -65,7 +65,7 @@ abstract class RooleElementMixin implements CustomElement {
         window.console.info("Reloading " + e.attributes["src"]);
         style = cache.style;
       }
-      return _compileRoole(text + "\n" + e.text, encaps, style).then((StyleElement el) {
+      return _compileRoole(text + "\n" + e.text, style).then((StyleElement el) {
         if (cache == null)
           cache = new _CachedStyle(el);
         cache.text = text;
@@ -76,40 +76,38 @@ abstract class RooleElementMixin implements CustomElement {
       window.console.log("Could not load " + e.attributes["src"]);
     });
   }
-  
-  Future<dynamic> _compileRooleElement(Element e, bool encaps) {
-    Future<_CachedStyle> ret = _compileRooleHTTP(e, encaps);
-    
+
+  Future<dynamic> _compileRooleElement(Element e) {
+    Future<_CachedStyle> ret = _compileRooleHTTP(e);
+
     if (e.attributes.containsKey("monitor")) {
       int period = int.parse(e.attributes["monitor"], onError: (_) => 0);
       if (period > 0)
         ret = ret.then((_CachedStyle cache) =>
-            new Timer.periodic(new Duration(seconds: period), (_) => _compileRooleHTTP(e, encaps, cache))
+            new Timer.periodic(new Duration(seconds: period), (_) => _compileRooleHTTP(e, cache))
         );
     }
-    
+
     return ret;
   }
-  
-  void compileRoole() {
-    bool encaps = _needEncapsulation();
 
+  void compileRoole() {
     this.host.classes.add("roole-wait");
-    
+
     List<Future> futures = new List();
-    
+
     this.shadowRoot.queryAll('roole').forEach((Element e) {
       e.remove();
       Future fut;
       if (e.attributes.containsKey("src")) {
-        fut = _compileRooleElement(e, encaps);
+        fut = _compileRooleElement(e);
       }
       else {
-        fut = _compileRoole(e.text, encaps);
+        fut = _compileRoole(e.text);
       }
       futures.add(fut);
     });
-    
+
     Future.wait(futures).then((e) {
       this.host.classes.remove("roole-wait");
     });
@@ -118,10 +116,10 @@ abstract class RooleElementMixin implements CustomElement {
 }
 
 class RooleElement extends PolymerElement with RooleElementMixin {
-  
+
   void created() {
     super.created();
     compileRoole();
   }
-  
+
 }
